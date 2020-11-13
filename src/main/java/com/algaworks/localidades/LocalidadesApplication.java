16fff89f1679cd1596eac143c9a4ba7d23519112
marcosfalves <marcosf_alves@outlook.com>
2020-11-13 05:56:21 -0300

    updated for Java 11 e Spring 2.3.3

--- src/main/java/com/algaworks/localidades/LocalidadesApplication.java
+++ src/main/java/com/algaworks/localidades/LocalidadesApplication.java
@@ -2,8 +2,6 @@ package com.algaworks.localidades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 @SpringBootApplication
 public class LocalidadesApplication {
@@ -11,12 +9,5 @@ public class LocalidadesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LocalidadesApplication.class, args);
 	}
-	
-	@ControllerAdvice
-    static class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
-        public JsonpAdvice() {
-            super("callback");
-        }
-    }
 
 }
